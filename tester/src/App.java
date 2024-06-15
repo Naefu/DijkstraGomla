edef5118ebba28178ea5d1ee033a268387fd174c
Naefu <ahmoslamy@hotmail.com>
2024-06-16 01:15:41 +0300

    helper for dijkstra

--- tester/src/App.java
+++ tester/src/App.java
@@ -2,71 +2,152 @@ import java.awt.Color;
 import java.awt.image.*;
 import java.io.*;
 import java.util.*;
+import java.util.stream.Collectors;
+
 import javax.imageio.*;
 public class App {
     public static void main(String[] args) throws IOException {
-        String[] groceries = {"milk","tea","towel","ice cream","bread","black pepper","tomatoes"};
-        Node nodeA = new Node("Dairy");
-        Node nodeAb = new Node("Dairy");
-        Node nodeAe = new Node("Dairy");
-        Node nodeB = new Node("Bread,Pastas");
-        Node nodeBb = new Node("Bread,Pastas");
-        Node nodeBe = new Node("Bread,Pastas");
-        Node nodeC = new Node("Tea");
-        Node nodeCb = new Node("Tea");
-        Node nodeCe = new Node("Tea");
-        Node nodeD = new Node("Spices");
-        Node nodeDb = new Node("Spices");
-        Node nodeDe = new Node("Spices");
-        Node nodeE = new Node("Cereal");
-        Node nodeEb = new Node("Cereal");
-        Node nodeEe = new Node("Cereal");
-        Node nodeF = new Node("Fresh Produce");
-        Node nodeFb = new Node("Fresh Produce");
-        Node nodeFe = new Node("Fresh Produce");
-        Node nodeG = new Node("Laundry");
-        Node nodeGb = new Node("Laundry");
-        Node nodeGe = new Node("Laundry");
-        Node nodeH = new Node("Ice cream");
-        Node nodeHb = new Node("Ice cream");
-        Node nodeHe = new Node("Ice cream");
+        // String[] groceries = {"milk","tea","towel","ice cream","bread","black pepper","tomatoes"};
+        // Node nodeA = new Node("Dairy");
+        // Node nodeAb = new Node("Dairy");
+        // Node nodeAe = new Node("Dairy");
+        // Node nodeB = new Node("Bread,Pastas");
+        // Node nodeBb = new Node("Bread,Pastas");
+        // Node nodeBe = new Node("Bread,Pastas");
+        // Node nodeC = new Node("Tea");
+        // Node nodeCb = new Node("Tea");
+        // Node nodeCe = new Node("Tea");
+        // Node nodeD = new Node("Spices");
+        // Node nodeDb = new Node("Spices");
+        // Node nodeDe = new Node("Spices");
+        // Node nodeE = new Node("Cereal");
+        // Node nodeEb = new Node("Cereal");
+        // Node nodeEe = new Node("Cereal");
+        // Node nodeF = new Node("Fresh Produce");
+        // Node nodeFb = new Node("Fresh Produce");
+        // Node nodeFe = new Node("Fresh Produce");
+        // Node nodeG = new Node("Laundry");
+        // Node nodeGb = new Node("Laundry");
+        // Node nodeGe = new Node("Laundry");
+        // Node nodeH = new Node("Ice cream");
+        // Node nodeHb = new Node("Ice cream");
+        // Node nodeHe = new Node("Ice cream");
         
-        nodeA.addAdjacentNode(nodeAb, 1);
-        nodeA.addAdjacentNode(nodeAe, 1);
-        nodeB.addAdjacentNode(nodeBb, 1);
-        nodeB.addAdjacentNode(nodeBe, 1);
-        nodeC.addAdjacentNode(nodeCb, 1);
-        nodeC.addAdjacentNode(nodeCe, 1);
-        nodeD.addAdjacentNode(nodeDb, 1);
-        nodeD.addAdjacentNode(nodeDe, 1);
-        nodeE.addAdjacentNode(nodeEb, 1);
-        nodeE.addAdjacentNode(nodeEe, 1);
-        nodeF.addAdjacentNode(nodeFb, 1);
-        nodeF.addAdjacentNode(nodeFe, 1);
-        nodeG.addAdjacentNode(nodeGb, 1);
-        nodeG.addAdjacentNode(nodeGe, 1);
-        nodeH.addAdjacentNode(nodeHb, 1);
-        nodeH.addAdjacentNode(nodeHe, 1);
+        // nodeA.addAdjacentNode(nodeAb, 1);
+        // nodeA.addAdjacentNode(nodeAe, 1);
+        // nodeB.addAdjacentNode(nodeBb, 1);
+        // nodeB.addAdjacentNode(nodeBe, 1);
+        // nodeC.addAdjacentNode(nodeCb, 1);
+        // nodeC.addAdjacentNode(nodeCe, 1);
+        // nodeD.addAdjacentNode(nodeDb, 1);
+        // nodeD.addAdjacentNode(nodeDe, 1);
+        // nodeE.addAdjacentNode(nodeEb, 1);
+        // nodeE.addAdjacentNode(nodeEe, 1);
+        // nodeF.addAdjacentNode(nodeFb, 1);
+        // nodeF.addAdjacentNode(nodeFe, 1);
+        // nodeG.addAdjacentNode(nodeGb, 1);
+        // nodeG.addAdjacentNode(nodeGe, 1);
+        // nodeH.addAdjacentNode(nodeHb, 1);
+        // nodeH.addAdjacentNode(nodeHe, 1);
+        // Dijkstra dijkstra = new Dijkstra();
+        // dijkstra.calculateShortestPath(nodeA);
+        // dijkstra.printPaths(Arrays.asList(nodeA, nodeAb));
+
+
+
+
+
+
+
+        Node nodeA = new Node("A");
+        Node nodeB = new Node("B");
+        Node nodeC = new Node("C");
+        Node nodeD = new Node("D");
+        Node nodeE = new Node("E");
+        Node nodeF = new Node("F");
+
+        nodeA.addAdjacentNode(nodeB, 2);
+        nodeA.addAdjacentNode(nodeC, 4);
+
+        nodeB.addAdjacentNode(nodeC, 3);
+        nodeB.addAdjacentNode(nodeD, 1);
+        nodeB.addAdjacentNode(nodeE, 5);
+
+        nodeC.addAdjacentNode(nodeD, 2);
+
+        nodeD.addAdjacentNode(nodeE, 1);
+        nodeD.addAdjacentNode(nodeF, 4);
+
+        nodeE.addAdjacentNode(nodeF, 2);
+
         Dijkstra dijkstra = new Dijkstra();
         dijkstra.calculateShortestPath(nodeA);
-        dijkstra.printPaths(Arrays.asList(nodeA, nodeB, nodeC));
+
+//START OF FUNCTION ---------------------------------------------------------------------
+List<Node> niggas = new LinkedList<>();
+// niggas.add(nodeA);
+niggas.add(nodeB);
+niggas.add(nodeC);
+niggas.add(nodeD);
+niggas.add(nodeE);
+niggas.add(nodeF);
+int shortnigga = 999999;
+int bignigga = 0;
+int whichnigga = 0;
+int whichernigga = 0;
+for(int i = 0;i<niggas.size();i++){
+    if(shortnigga > niggas.get(i).getDistance()){
+        shortnigga = niggas.get(i).getDistance();
+        whichnigga = i+1;
+    }
+    if(bignigga < niggas.get(i).getDistance()){
+        bignigga = niggas.get(i).getDistance();
+        whichernigga = i;
+    }
+    
+}
+
+System.out.println(whichnigga);
+System.out.println(shortnigga); 
+System.out.println(whichernigga);
+System.out.println(bignigga);     
+//END OF FUNCTION -------------------------------------------------------------------------
+        dijkstra.printPaths(List.of(nodeA, nodeB, nodeC, nodeD, nodeE, nodeF));
+
+
+
+
+
+
+
+
+
+
+
 
 
 BufferedImage img = ImageIO.read(new File("../DijkstraGomla/tester/src/imageinput.jpg"));
-var g = img.createGraphics();
-g.setColor(new Color(90,90,90));
-// Draw on the BufferedImage via the graphics context.
-int x = 0;
-int y = 0;
-int width = 100;
-int height = 1000;
-g.drawLine(10, 10, 500, 500);
+var a = img.createGraphics();
+a.setColor(new Color(90,90,90));
+a.fillRect(250, 205, 173, 4);
+var b = img.createGraphics();
+b.setColor(new Color(100,0,90));
+b.fillRect(423, 205, 173, 4);
+var c = img.createGraphics();
+c.setColor(new Color(90,0,0));
+c.fillRect(557, 205, 173, 4);
+var d = img.createGraphics();
+d.setColor(new Color(0,90,90));
+d.fillRect(556, 117, 4, 90);
 File f = new File("../DijkstraGomla/tester/src/image.jpg");
 		try {
 			ImageIO.write(img, "jpg", f);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-g.dispose();
+        a.dispose();
+        b.dispose();
+        c.dispose();
+        d.dispose();
     }
 }
